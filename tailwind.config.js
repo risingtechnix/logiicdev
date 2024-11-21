543d2436e94944feb47e8056be07b25432c77ce8
amarpakhare <amarpakhare30@gmail.com>
2024-11-21 22:29:20 +0530

    testing the image path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import IOT from "./src/assets/Solution/IOT_Pencil_draing.webp"
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	darkMode: "class",
@@ -24,7 +25,8 @@ export default {
 				poppins: ["Poppins", "sans-serif"],
 			},
 			backgroundImage: {
-				iot: "url('src/assets/Solution/IOT_Pencil_draing.webp')",
+				iot: "url('./src/assets/Solution/IOT_Pencil_draing.webp')",
+				// iot: IOT,
 				energyEfficiency: "url('src/assets/Solution/Energy Efficiency.webp')",
 				drones: "url('src/assets/Solution/drone.jpeg')",
 				mobility: "url('src/assets/Solution/mobility_green.jpeg')",
